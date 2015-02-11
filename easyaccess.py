664f161dd70585885a018acb0a4a6bf19a95e878
Matias Carrasco Kind <mgckind@gmail.com>
2015-02-10 21:53:36 -0600

    Added more features

--- easyaccess.py
+++ easyaccess.py
@@ -28,15 +28,6 @@ import pandas as pd
 
 
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 #readline.parse_and_bind('tab: complete')
 
@@ -108,7 +99,7 @@ class easy_or(cmd.Cmd, object):
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.table_restriction_clause = " "
-        self.savePrompt = 'DESDM >> '
+        self.savePrompt = colored('DESDM >> ','cyan')
         self.prompt = self.savePrompt
         self.pipe_process_handle = None
         self.buff = None
