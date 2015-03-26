b0d75141eb37659b98c9c573ec532456cfbfac5f
Matias Carrasco Kind <mgckind@gmail.com>
2015-03-26 13:55:19 -0500

    setuptools  fix

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import sys
 import os
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
+#try:
+#from setuptools import setup, find_packages
+#except ImportError:
+from distutils.core import setup
 prjdir = os.path.dirname(__file__)
 
 def read(filename):
@@ -20,10 +20,11 @@ setup(
     author_email='mcarras2@illinois.edu',
     scripts=['easyaccess'],
     py_modules=['easyaccess','config','eautils.des_logo'],
-    packages=find_packages(),
+    #packages=find_packages(),
+    packages=['eautils'],
     license='NCSA',
     description='Easy Access to access DES DB',
     long_description=read('README.md'),
     url='https://github.com/mgckind/easyaccess',
-    install_requires=['pandas >= 0.14','termcolor','pyfits >= 3.3','cx_Oracle'],
+    #install_requires=['pandas >= 0.14','termcolor','pyfits >= 3.3','cx_Oracle'],
 )
