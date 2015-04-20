397fe46efcd4f2bd9273553253320133e862de43
Matias Carrasco Kind <mgckind@gmail.com>
2015-04-20 10:53:26 -0500

    Fix enconding

--- eautils/des_logo.py
+++ eautils/des_logo.py
@@ -1,4 +1,5 @@
 __author__ = 'Matias Carrasco Kind'
+import sys
 try:
     from termcolor import colored as c
 except:
@@ -8,20 +9,27 @@ except:
 def noc(line,color):
     return line
 def print_deslogo(color=True):
+    char0=u"\u203E"
+    char1=u"\u203E"
+    char2=u"\u00B4"
+    if sys.stdout.encoding != 'UTF-8':
+        char0=' '
+        char1='-'
+        char2='`'
     if color:
         c2=c
     else:
         c2=noc
     L=[]
-    #L.append("     ______      ")
-    L.append("""     \\"""+u"\u203E"*6+"""\      """)
+    if sys.stdout.encoding != 'UTF-8': L.append("     _______      ")
+    L.append("""     \\"""+char0*6+"""\      """)
     L.append("  "+c2("//","red")+" / .    .\    ")
     L.append(" "+c2("//","red")+" /   .    _\   ")
     L.append(c2("//","red")+" /  .     / "+c2("//","red")+" ")
     L.append(c2("\\\\","red")+" \     . / "+c2("//","red")+"  ")
     L.append(c2(" \\\\","red")+" \_____/ "+c2("//","red")+"   ")
     L.append(c2("  \\\\_______//","red")+"    DARK ENERGY SURVEY")
-    last=c2("""   `"""+u"\u203E"*7+u"\u00B4","red") +"     DATA MANAGEMENT"
+    last=c2("""   `"""+char1*7+char2,"red") +"     DATA MANAGEMENT"
     L.append(last)
 
     print 
