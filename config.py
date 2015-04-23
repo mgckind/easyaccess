fcf1bcea26518d8b9fb65086ef3a395e51e83483
Matias Carrasco Kind <mgckind@gmail.com>
2015-04-23 09:57:54 -0500

    Reformatting...

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-#Config file
+# Config file
 import ConfigParser
 import getpass
 import time
@@ -6,7 +6,7 @@ import sys
 import cx_Oracle
 import os
 
-configcomment="""#
+configcomment = """#
 # Easyaccess default parameters
 # 
 # database  : Default is dessci, change to desoper, destest and others
@@ -20,11 +20,12 @@ configcomment="""#
 # nullvalue : The value used to replace null or empty entries when printing into a file
 """
 
-descomment="""#
+descomment = """#
 # DES services configuration
 # Please modify the passwords accordingly
 #
 """
+
 
 def get_config(configfile):
     """
@@ -33,33 +34,35 @@ def get_config(configfile):
     """
     config = ConfigParser.ConfigParser()
     configwrite = False
-    check=config.read(configfile)
+    check = config.read(configfile)
     if check == []:
-        configwrite= True
-        print '\nCreating a configuration file... at %s\n' %configfile
+        configwrite = True
+        print '\nCreating a configuration file... at %s\n' % configfile
 
     if not config.has_section('easyaccess'):
         configwrite = True
         config.add_section('easyaccess')
-    if not config.has_option('easyaccess','database'): configwrite = True ;config.set('easyaccess','database','dessci')
-    if not config.has_option('easyaccess','editor'): configwrite = True ;config.set('easyaccess','editor','nano')
-    if not config.has_option('easyaccess','prefetch'): configwrite = True ;config.set('easyaccess','prefetch',10000)
-    if not config.has_option('easyaccess','histcache'): configwrite = True ;config.set('easyaccess','histcache',5000)
-    if not config.has_option('easyaccess','timeout'): configwrite = True ;config.set('easyaccess','timeout',900)
-    if not config.has_option('easyaccess','nullvalue'): configwrite = True ;config.set('easyaccess','nullvalue',-9999)
-    
+    if not config.has_option('easyaccess', 'database'): configwrite = True;config.set('easyaccess', 'database',
+                                                                                      'dessci')
+    if not config.has_option('easyaccess', 'editor'): configwrite = True;config.set('easyaccess', 'editor', 'nano')
+    if not config.has_option('easyaccess', 'prefetch'): configwrite = True;config.set('easyaccess', 'prefetch', 10000)
+    if not config.has_option('easyaccess', 'histcache'): configwrite = True;config.set('easyaccess', 'histcache', 5000)
+    if not config.has_option('easyaccess', 'timeout'): configwrite = True;config.set('easyaccess', 'timeout', 900)
+    if not config.has_option('easyaccess', 'nullvalue'): configwrite = True;config.set('easyaccess', 'nullvalue', -9999)
+
     if not config.has_section('display'):
-        configwrite=True
+        configwrite = True
         config.add_section('display')
-    if not config.has_option('display','color_terminal'): configwrite = True ;config.set('display','color_terminal','yes')
-    if not config.has_option('display','loading_bar'): configwrite = True ;config.set('display','loading_bar','yes')
-    if not config.has_option('display','max_rows'): configwrite = True ;config.set('display','max_rows',1500)
-    if not config.has_option('display','width'): configwrite = True ;config.set('display','width',1000)
-    if not config.has_option('display','max_columns'): configwrite = True ;config.set('display','max_columns',50)
-    
+    if not config.has_option('display', 'color_terminal'): configwrite = True;config.set('display', 'color_terminal',
+                                                                                         'yes')
+    if not config.has_option('display', 'loading_bar'): configwrite = True;config.set('display', 'loading_bar', 'yes')
+    if not config.has_option('display', 'max_rows'): configwrite = True;config.set('display', 'max_rows', 1500)
+    if not config.has_option('display', 'width'): configwrite = True;config.set('display', 'width', 1000)
+    if not config.has_option('display', 'max_columns'): configwrite = True;config.set('display', 'max_columns', 50)
+
     check = True
     if configwrite == True:
-        check=write_config(configfile, config)
+        check = write_config(configfile, config)
         config.read(configfile)
     if check:
         return config
@@ -70,8 +73,8 @@ def write_config(configfile, config_ob):
     Writes configuration file
     """
     try:
-        F=open(configfile,'w')
-        F.write(configcomment+'\n')
+        F = open(configfile, 'w')
+        F.write(configcomment + '\n')
         config_ob.write(F)
         F.flush()
         F.close()
@@ -81,81 +84,83 @@ def write_config(configfile, config_ob):
         return False
 
 
-def get_desconfig(desfile,db):
+def get_desconfig(desfile, db):
     """
     Loads des config file or create one if not
 
     """
-    server_n='leovip148.ncsa.uiuc.edu'
-    port_n='1521'
+    server_n = 'leovip148.ncsa.uiuc.edu'
+    port_n = '1521'
 
-    if not db[:3] == 'db-': db='db-'+db
+    if not db[:3] == 'db-': db = 'db-' + db
     config = ConfigParser.ConfigParser()
     configwrite = False
-    check=config.read(desfile)
+    check = config.read(desfile)
     if check == []:
-        configwrite= True
+        configwrite = True
         print '\nError in DES_SERVICES config file, creating a new one...'
         print 'File might not exists or is not configured'
-        print 
+        print
 
-    databases = ['db-desoper','db-dessci','db-destest'] #most used ones anyways
+    databases = ['db-desoper', 'db-dessci', 'db-destest']  #most used ones anyways
 
     if db not in databases and not config.has_section(db):
-        check_db=raw_input('\nDB entered not dessci, desoper or destest or in DES_SERVICE file, continue anyway [y]/n\n')
-        if check_db in ('n','N','no','No','NO'): sys.exit(0)
+        check_db = raw_input(
+            '\nDB entered not dessci, desoper or destest or in DES_SERVICE file, continue anyway [y]/n\n')
+        if check_db in ('n', 'N', 'no', 'No', 'NO'): sys.exit(0)
 
     if not config.has_section(db):
-        print '\nAdding section %s to des_service file\n' % db 
+        print '\nAdding section %s to des_service file\n' % db
         configwrite = True
         kwargs = {'host': server_n, 'port': port_n, 'service_name': db[3:]}
         dsn = cx_Oracle.makedsn(**kwargs)
-        good=False
+        good = False
         for i in range(3):
             try:
-                user=raw_input('Enter username : ')
-                pw1=getpass.getpass(prompt='Enter password : ')
-                ctemp=cx_Oracle.connect(user,pw1,dsn=dsn)
+                user = raw_input('Enter username : ')
+                pw1 = getpass.getpass(prompt='Enter password : ')
+                ctemp = cx_Oracle.connect(user, pw1, dsn=dsn)
                 good = True
                 break
             except:
                 (type, value, traceback) = sys.exc_info()
                 print value
-                if value.message.code == 1017: pass    
-                else: sys.exit(0)
-        if good: 
+                if value.message.code == 1017:
+                    pass
+                else:
+                    sys.exit(0)
+        if good:
             ctemp.close()
         else:
             print '\n Check your credentials and/or database access\n'
             sys.exit(0)
         config.add_section(db)
-        
-    if not config.has_option(db,'user'): configwrite = True ;config.set(db,'user',user)
-    if not config.has_option(db,'passwd'): configwrite = True ;config.set(db,'passwd',pw1)
-    if not config.has_option(db,'name'): configwrite = True ;config.set(db,'name',db[3:])
-    if not config.has_option(db,'server'): configwrite = True ;config.set(db,'server',server_n)
-    if not config.has_option(db,'port'): configwrite = True ;config.set(db,'port',port_n)
 
-    
+    if not config.has_option(db, 'user'): configwrite = True;config.set(db, 'user', user)
+    if not config.has_option(db, 'passwd'): configwrite = True;config.set(db, 'passwd', pw1)
+    if not config.has_option(db, 'name'): configwrite = True;config.set(db, 'name', db[3:])
+    if not config.has_option(db, 'server'): configwrite = True;config.set(db, 'server', server_n)
+    if not config.has_option(db, 'port'): configwrite = True;config.set(db, 'port', port_n)
+
     check = True
     if configwrite == True:
-        check=write_desconfig(desfile, config)
+        check = write_desconfig(desfile, config)
         config.read(desfile)
     if check:
         return config
-    
+
 
 def write_desconfig(configfile, config_ob):
     """
     Writes configuration file
     """
     try:
-        F=open(configfile,'w')
-        F.write(descomment+'\n')
+        F = open(configfile, 'w')
+        F.write(descomment + '\n')
         config_ob.write(F)
         F.flush()
         F.close()
-        os.chmod(configfile,2**8+2**7) #rw-------  
+        os.chmod(configfile, 2 ** 8 + 2 ** 7)  #rw-------
         return True
     except:
         print "Problems writing the configuration  file %s" % configfile
