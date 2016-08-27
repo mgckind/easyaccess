57e86ec5efc8980ba809e88ae0f42be021bbbede
Matias Carrasco Kind <mgckind@gmail.com>
2016-08-27 17:28:42 -0500

    update version

--- easyaccess/version.py
+++ easyaccess/version.py
@@ -17,7 +17,7 @@ warnings.filterwarnings("ignore")
 #    versions.sort()
 #    return versions[-1]
 
-version_tag = (1, 3, 2, 'dev-4a03bdd')
+version_tag = (1, 3, 2, 'dev-5cde1b8')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
