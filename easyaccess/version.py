36219beffb2fb19bf89e907295d89bcdf7977d13
Matias Carrasco Kind <mgckind@gmail.com>
2016-09-27 12:32:23 -0500

    dev version d5ca024

--- easyaccess/version.py
+++ easyaccess/version.py
@@ -17,7 +17,7 @@ warnings.filterwarnings("ignore")
 #    versions.sort()
 #    return versions[-1]
 
-version_tag = (1, 3, 2, 'dev-0a28d4b')
+version_tag = (1, 3, 2, 'dev-d5ca024')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
