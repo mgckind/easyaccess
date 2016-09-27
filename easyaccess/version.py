6f6993010903502125f4ad9b5b9cd9d5fce4b299
Matias Carrasco Kind <mgckind@gmail.com>
2016-09-27 12:37:19 -0500

    dev version fd41ecc

--- easyaccess/version.py
+++ easyaccess/version.py
@@ -17,7 +17,7 @@ warnings.filterwarnings("ignore")
 #    versions.sort()
 #    return versions[-1]
 
-version_tag = (1, 3, 2, 'dev-d5ca024')
+version_tag = (1, 3, 2, 'dev-fd41ecc')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
