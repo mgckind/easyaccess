9d18c22cc0d642a31e4fc8af5cbbdf60f1e81af9
Matias Carrasco Kind <mgckind@gmail.com>
2017-04-05 16:49:22 -0500

    1.4.0 rc1

--- easyaccess/version.py
+++ easyaccess/version.py
@@ -19,10 +19,8 @@ def last_pip_version():
         uploads.append([k,datetime.strptime(up_time,'%Y-%m-%dT%H:%M:%S')])
     return sorted(uploads,key=lambda x: x[1])[-1][0]
 
-version_tag = (1, 4, 0, 'dev-a9c5529')
+version_tag = (1, 4, 0, 'dev-rc1')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
     __version__ = '%s-%s' % (__version__, version_tag[3])
-
-
