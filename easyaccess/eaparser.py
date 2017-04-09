6e3ed6e58e5f5eab4e1920e828daba1a07bd66aa
Matias Carrasco Kind <mgckind@gmail.com>
2017-04-08 22:12:46 -0500

    clean up

--- easyaccess/eaparser.py
+++ easyaccess/eaparser.py
@@ -3,6 +3,8 @@ import argparse
 import config_ea as config_mod
 from version import __version__
 import sys
+import os
+
 
 class MyParser(argparse.ArgumentParser):
     def error(self, message):
@@ -16,8 +18,9 @@ class MyParser(argparse.ArgumentParser):
 def get_args(config_file):
     conf = config_mod.get_config(config_file)
     parser = MyParser(
-        description='Easy access to the DES database. There is a configuration file located in %s for more customizable options' % config_file)
-    parser.add_argument("-v", "--version", action = "store_true",
+        description='Easy access to the DES database. There is a configuration file '
+                    'located in %s for more customizable options' % config_file)
+    parser.add_argument("-v", "--version", action="store_true",
                         help="print version number and exit")
     parser.add_argument("-c", "--command", dest='command',
                         help="Executes command and exit")
@@ -34,27 +37,28 @@ def get_args(config_file):
     parser.add_argument("--tablename", dest='tablename',
                         help="Custom table name to be used with --load_table\
                         or --append_table")
-    parser.add_argument("--chunksize", dest='chunksize', type=int, default = None,
-                        help="Number of rows to be inserted at a time. Useful for large files \
-                                        that do not fit in memory. Use with --load_table or --append_table")
-    parser.add_argument("--memsize", dest='memsize', type=int, default = None,
+    parser.add_argument("--chunksize", dest='chunksize', type=int, default=None,
+                        help="Number of rows to be inserted at a time. Useful for large files "
+                             "that do not fit in memory. Use with --load_table or --append_table")
+    parser.add_argument("--memsize", dest='memsize', type=int, default=None,
                         help=" Size of chunk to be read at a time in Mb. Use with --load_table or "
                              "--append_table")
-    parser.add_argument("-s", "--db",dest='db',choices=['dessci', 'desoper', 'destest'],
+    parser.add_argument("-s", "--db", dest='db', choices=['dessci', 'desoper', 'destest'],
                         help="Override database name [dessci,desoper,destest]")
     parser.add_argument("-q", "--quiet", action="store_true", dest='quiet',
                         help="Silence initialization, no loading bar")
     parser.add_argument("-u", "--user", dest='user')
     parser.add_argument("-p", "--password", dest='password')
     parser.add_argument("-nr", "--no_refresh", dest='norefresh', action="store_true",
-                        help="Do not refresh metadata at starting up to speed initialization. Metadata can "
-                             "always be refreshed from inside using the refresh_metadata command")
+                        help="Do not refresh metadata at starting up to speed initialization. "
+                             "Metadata can always be refreshed from inside using the "
+                             "refresh_metadata command")
     parser.add_argument("--config", help="--config show, will print content of "
-                                                        "config file\n"
-                                                        "--config reset will reset config to default "
-                                                        "values\n"
-                                                        "--config set param1=val1 param2=val2 will "
-                                                        "modify parameters for the session only", nargs='+')
+                        "config file\n"
+                        "--config reset will reset config to default "
+                        "values\n"
+                        "--config set param1=val1 param2=val2 will "
+                        "modify parameters for the session only", nargs='+')
     args = parser.parse_args()
 
     if args.version:
@@ -62,8 +66,6 @@ def get_args(config_file):
         sys.exit()
 
     if args.config:
-        int_keys = ['prefetch', 'histcache', 'timeout', 'max_rows', 'width', 'max_columns', 'outfile_max_mb',
-                        'nullvalue', 'loading_bar', 'autocommit', 'max_col_width']
         if args.config[0] == 'show':
             print('\n Showing content of the config file (%s) :\n' % config_file)
             file_temp = open(config_file, 'r')
@@ -83,10 +85,11 @@ def get_args(config_file):
                 parser.print_help()
                 sys.exit()
             entries = ','.join(args.config[1:])
-            entries = entries.replace(',,',',')
+            entries = entries.replace(',,', ',')
             entries = entries.split(',')
             for e in entries:
-                if e =='': continue
+                if e == '':
+                    continue
                 updated = False
                 try:
                     key, value = e.split('=')
@@ -94,11 +97,11 @@ def get_args(config_file):
                         if conf.has_option(section, key):
                             conf.set(section, key, str(value))
                             updated = True
-                    if not updated: raise
+                    if not updated:
+                        raise
                 except:
-                    print("Check the key exists or that you included the '=' for the parameter\nFor more "
-                          "info "
-                          "use --help.")
+                    print("Check the key exists or that you included the '=' for the "
+                          "parameter\nFor more info use --help.")
                     sys.exit()
         else:
             parser.print_help()
