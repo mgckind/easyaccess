97f332ce66a24df5f54c16dc527bb6be281252c3
audreykoz <audrey@koziol.cc>
2018-07-05 13:15:05 -0500

    Changing version to dev-1ff3a0d

--- easyaccess/version.py
+++ easyaccess/version.py
@@ -24,7 +24,7 @@ def last_pip_version():
     return sorted(uploads, key=lambda x: x[1])[-1][0]
 
 
-version_tag = (1, 4, 5, 'dev-fa91174')
+version_tag = (1, 4, 5, 'dev-1ff3a0d')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
