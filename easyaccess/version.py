edd4b936324075e4b1a1756e924ae6e7593a0c16
audreykoz <audrey@koziol.cc>
2018-08-10 09:40:16 -0500

    Changed version to dev-f7b4cc7

--- easyaccess/version.py
+++ easyaccess/version.py
@@ -24,7 +24,7 @@ def last_pip_version():
     return sorted(uploads, key=lambda x: x[1])[-1][0]
 
 
-version_tag = (1, 4, 5, 'dev-fda8df5')
+version_tag = (1, 4, 5, 'dev-f7b4cc7')
 __version__ = '.'.join(map(str, version_tag[:3]))
 
 if len(version_tag) > 3:
